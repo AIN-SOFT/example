e298ad6d38546cfb804f40793601a8a83b94ce8a
13485099125@163.com <xupeng1841>
2019-03-25 06:28:18 +0800

    stream

---
+++ src/week2/FlowLayoutposition.java
@@ -0,0 +1,35 @@
+package week2;
+/**
+ * 流布局的使用
+ * @auther xupeng
+ * 2019.3.25
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutposition extends JFrame {
+    public FlowLayoutposition(){
+        setTitle("流布局的使用");
+        Container c =getContentPane();
+        setLayout( new FlowLayout(FlowLayout.CENTER,10,10));
+        JLabel jLabel = new JLabel("下面四句诗，哪一个是描写夏天的？");
+        JLabel jLabe2= new JLabel("A.秋风萧瑟天气凉，树木摇荡露为霜。");
+        JLabel jLabe3 = new JLabel("B.白雪纷纷何所似，撒盐空中差可似。");
+        JLabel jLabe4 = new JLabel("C.接天莲叶无穷碧，映日荷花别样红。");
+        JLabel jLabe5 = new JLabel("D.竹外桃花两三枝，春江水暖鸭先知。");
+        c.add(jLabel);
+        c.add(jLabe2);
+        c.add(jLabe3);
+        c.add(jLabe4);
+        c.add(jLabe5);
+        setSize(300,300);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FlowLayoutposition();
+    }
+}
